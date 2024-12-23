31482ab745b20427633e4bb00911206dc2327e7a
Marco Bakera <marco@bakera.de>
2024-12-23 13:40:06 +0100

    add RotatingPlasmaDemo class to enhance demo functionality

--- demos.py
+++ demos.py
@@ -68,13 +68,39 @@ class Plasma:
                    math.cos((y * self.s) + self.i / 4.0))
         return v > 0.3
 
+class RotatingPlasmaDemo:
+    def __init__(self):
+        self.current = time.time()
+
+    def update(self):
+        self.current = time.time()
+
+    def draw(self):
+        pyxel.cls(0)
+
+        for y in range(HEIGHT):
+            for x in range(WIDTH):
+                b = self.handle_px(x, y)
+                col = int(b * 3)
+                if b:
+                    pyxel.pset(x, y, col)
+
+    def handle_px(self, x, y):
+        v = math.sin(1*(0.5*x*math.sin(self.current/2) +
+                        0.5*y*math.cos(self.current/3)) + self.current)
+        # -1 < sin() < +1
+        # therfore correct the value and bring into range [0, 1]
+        v = (v+1.0) / 2.0
+        return v 
+
+
 class DemoHandler:
     def __init__(self):
         self.timestep = 0
         self.parameter1 = 0        
         self.current_demo = 0
         self.demos = [
-            SwirlDemo(), Plasma()
+            SwirlDemo(), Plasma(), RotatingPlasmaDemo()
         ]
         pyxel.init(WIDTH, HEIGHT)
         pyxel.run(self.update, self.draw)
