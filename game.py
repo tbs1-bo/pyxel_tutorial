4a6379063f2754acccd067e5cd0483051a800817
Marco Bakera <marco@bakera.de>
2025-01-11 12:37:41 +0100

    refactor tilemap layout and player debug output handling

--- game.py
+++ game.py
@@ -1,12 +1,14 @@
 import pyxel
 
-# Tilemap layout
-# EMPTY  PLAYER
-# WALL   COIN
-EMPTY = 0,0
-PLAYER = 1,0
-WALL = 0,1
-COIN = 1,1
+
+class Tile:
+    # Tilemap layout
+    # EMPTY  PLAYER
+    # WALL   COIN
+    EMPTY = 0,0
+    PLAYER = 1,0
+    WALL = 0,1
+    COIN = 1,1
 
 
 class Player:
@@ -14,11 +16,13 @@ class Player:
         self.xy = x,y
         self.width = 8
         self.height = 8
+        self.debug_output = False
 
     def draw(self):
         px, py = self.xy
         pyxel.blt(px, py, 0, 8, 0, self.width, self.height)
-        pyxel.text(0, 0, "x: %d, y: %d" % (self.xy), 1)
+        if self.debug_output:
+            pyxel.text(0, 0, "x: %d, y: %d" % (self.xy), 1)
 
     def move(self):
         newx, newy = self.xy
@@ -35,11 +39,12 @@ class Player:
         return newx, newy
 
 class Game:
+
     def __init__(self):
         self.player = Player(9, 8)
 
         pyxel.init(8*8, 8*8)
-        pyxel.load("my_resource.pyxres")
+        pyxel.load("game.pyxres")
         pyxel.run(self.update, self.draw)
 
     def update(self):
@@ -52,16 +57,13 @@ class Game:
             pyxel.play(1,1)
             print("coin found")
             px, py = self.player.xy
-            # TODO currently not working
-            pyxel.tilemaps[0].pset(px//8, py//8, EMPTY)
+            pyxel.tilemaps[0].pset(px//8, py//8, Tile.EMPTY)
 
     def coin_found(self):
         tm = pyxel.tilemaps[0]
         px, py = self.player.xy
-        COIN = 1,1
-        LEVEL_OFFSET = 8, 0
-        tile = tm.pget(px//8 + LEVEL_OFFSET[0], py//8 + LEVEL_OFFSET[1])
-        return tile == COIN
+        tile = tm.pget(px//8, py//8)
+        return tile == Tile.COIN
 
     def inside(self, x, y):
         return 8 <= x <= 48 and 8 <= y <= 48
@@ -71,7 +73,7 @@ class Game:
 
         # tilemap
         # bltm(x, y, tm, u, v, w, h, [colkey], [rotate], [scale])
-        pyxel.bltm(0, 0, 0, 8*8,0, 8*8,8*8)
+        pyxel.bltm(0, 0, 0, 0,0, 8*8,8*8)
 
         # blt(x, y, img, u, v, w, h, [colkey], [rotate], [scale])
         self.player.draw()
